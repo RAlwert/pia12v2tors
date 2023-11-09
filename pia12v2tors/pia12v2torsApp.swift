df795ed4c6f0145603c9719faa378c423a4f6ddb
Robin <125597946+RAlwert@users.noreply.github.com>
2023-11-09 09:13:27 +0100

    Initial Commit

---
+++ pia12v2tors/pia12v2torsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pia12v2torsApp.swift
+//  pia12v2tors
+//
+//  Created by Robin Alwert on 2023-11-09.
+//
+
+import SwiftUI
+
+@main
+struct pia12v2torsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
